b2d8dcae648eaabe61d7fc830029d97b895fe6bf
Ben-Legg <benlegg2002@gmail.com>
2024-01-23 15:47:44 +0000

    Added reading data from polls.JSON when connecting to /

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+// Import modules --------------
 const express = require('express');
-const app = express();// start an express app
+const fs = require('fs')
 
-app.set('view engine', 'ejs');// regiser view engine
-
-app.listen(3000); // listen for requests, automatically assume address=localhost
-
+// Server setup --------------
+const app = express(); // start an express app
+app.set('view engine', 'ejs'); // regiser view engine
+app.listen(3000); // listen for requests on port 3000, automatically assume address=localhost
 app.use(express.static('public')); // static files for browser are in "public" folder
 
 app.use((req, res, next) => { // log information about user requests
@@ -16,16 +17,44 @@ app.use((req, res, next) => { // log information about user requests
 });
 
 
-// Routing requests
-
+// Routing requests --------------
 app.get('/', (req, res) => { // routes requests for "/" to our poll
     const title = 'Poll';
-    const choices = [
-        {option: 'Option One'},
-        {option: 'Option Two'},
-        {option: 'Option Three'},
-    ];
-    res.render('poll', {choices, title});
+    const pollId = 1; // Choose poll we want to quiz people on by pollId
+    let choices = [];
+    let question = '';
+    try {
+        const allPollsData = JSON.parse(fs.readFileSync('data/polls.json')); // Read poll data from file and parse to JSON
+        const pollData = allPollsData.find(poll => poll.pollId === pollId); // Find data object by pollId
+        
+        if (pollData) { // save poll data if poll with desired ID is found
+            question = pollData.question;
+
+            // Seach through option array to find poll options
+            const optionOne = pollData.options.find(option => option.optionId === 1);
+            const optionTwo = pollData.options.find(option => option.optionId === 2);
+            const optionThree = pollData.options.find(option => option.optionId === 3);
+
+            if (optionOne && optionTwo && optionThree) { // save option text if poll has three options
+                choices = [
+                    {option: optionOne.optionText},
+                    {option: optionTwo.optionText},
+                    {option: optionThree.optionText},
+                ];
+            } else {
+                console.log(`Options not found for pollId: ${pollId}.`);
+            }
+        } else {
+            console.log(`Poll with pollId: ${pollId} not found.`);
+        };
+        
+        res.render('poll', {choices, title, question});
+        
+    } catch (error) {
+        console.error('Error loading or parsing poll data:', error);
+        res.status(500).render('poll', {choices, title});
+    }
+    
 });
 
 app.get('/results', (req, res) => { // routes requests for "/results to our results page
